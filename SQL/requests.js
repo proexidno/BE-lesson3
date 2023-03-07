f14d99dd03b13db16ee841895a5d6f94bfe8d26a
proexidno <57622546+proexidno@users.noreply.github.com>
2023-03-08 01:34:32 +0700

    03.05.23

--- SQL/requests.js
+++ SQL/requests.js
@@ -1,5 +1,7 @@
 const sqlite3 = require("sqlite3");
-let db = new sqlite3.Database("test.db", (e) => {
+
+
+let db = new sqlite3.Database("warehouse.db", (e) => {
     if (e) {
         logger(e)
     } else {
@@ -11,18 +13,37 @@ function logger(e) {
     console.log(`\nERROR occured:\n${e}\n`);
 }
 
-
-db.run('CREATE TABLE IF NOT EXISTS users (name TEXT, pass TEXT)');
-
-
-db.run('INSERT INTO users (name, pass) VALUES (?, ?)', ["Софья", "admin1"], (e) => {
-    if (e) {
-        logger(e)
-    } else {
-        console.log("All's OK\n");
-    }
+db.serialize(() => {
+    db.run(`
+    CREATE TABLE IF NOT EXISTS parts (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE,
+        price INTEGER NOT NULL
+    )
+    `)
+    db.run(`
+    CREATE TABLE IF NOT EXISTS providers (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        lastname TEXT,
+        adress TEXT NOT NULL
+    )
+    `)
+    db.run(`
+    CREATE TABLE IF NOT EXISTS storage (
+        id INTEGER PRIMARY KEY,
+        is_in_storage BOOL NOT NULL,
+        count INTEGER,
+        part_id INTEGER,
+        provider_id INTEGER,
+        FOREIGN KEY (part_id) REFERENCES parts (id),
+        FOREIGN KEY (provider_id) REFERENCES providers (id)
+    )
+    `)
+    
 })
 
+db.run(`SELECT "Hello world"`)
 
 db.close();
 
